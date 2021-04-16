852887554398c1f2853f11e87e4d084703c169d4
akashfit2max <akashnikz@gmail.com>
2021-04-16 21:24:47 +0530

    reverse array

--- DS-ALGO/src/Handson/ArraysDS.java
+++ DS-ALGO/src/Handson/ArraysDS.java
@@ -1,9 +1,24 @@
 package Handson;
 
+import java.util.Scanner;
+
 public class ArraysDS {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no of terms");
+		int n = sc.nextInt();
+
+		System.out.println("Enter the terms");
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("reversed array");
+
+		for (int i = n - 1; i >= 0; i--) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 
 }
